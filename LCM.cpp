c63e49af91ea8c5b6f815ae003d1212317667ec5
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2021-11-24 20:04:11 +0530

    LCM of two numbers

---
+++ LCM.cpp
@@ -0,0 +1,21 @@
+//LCM of two numbers - Recursive Method (Efficient Method)
+#include<iostream>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b==0)
+    return a;
+    else
+    return gcd(b,a%b);
+}
+int lcm(int a,int b)
+{
+return (a*b)/gcd(a,b);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<lcm(a,b);
+  return 0;
+}
