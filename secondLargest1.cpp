1c25012bd9df261cd2d3272e809e6ad19a57443f
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-01-18 14:48:29 +0530

    Second Largest element in an array -Efficient Soln

---
+++ secondLargest1.cpp
@@ -0,0 +1,30 @@
+//Second Largest element in an array - Efficient Solution
+//Time Complexity = theta(n)
+//Auxiliary Space = O(1)
+#include<iostream>
+using namespace std;
+int secondLargest(int arr[], int n)
+{
+    int res=-1, largest=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>arr[largest])
+        {
+            res=largest;
+            largest=i;
+        }
+    }
+    return res;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<secondLargest(arr,n);
+    return 0;
+}
