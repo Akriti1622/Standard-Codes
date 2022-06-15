ebb6eb9c1dc7c257e3b2be35f71a39ed84728375
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-15 18:37:47 +0530

    leftmost non-repeating character efficient soln 1

---
+++ leftmost non-repeating character efficient solution 1.cpp
@@ -0,0 +1,23 @@
+//leftmost non-repeating character efficient solution 1
+
+//leftmost repeating character efficient solution 1
+
+#include <bits/stdc++.h>
+using namespace std;
+const int CHAR=256;
+int Leftmost(string &s)
+{
+    int count[CHAR]={0};
+    for(int i=0;i<s.length();i++)
+        count[s[i]]++;
+    for(int i=0;i<s.length();i++)
+        if(count[s[i]]==1)
+            return i;
+    return -1;
+}
+int main() {
+    string s;
+    getline(cin, s);
+    cout<<Leftmost(s);
+    return 0;
+}
