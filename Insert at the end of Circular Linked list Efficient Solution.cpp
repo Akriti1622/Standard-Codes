ba0c28d4b87c271a711641bfc8ff31cf6a425fb6
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-23 12:55:51 +0530

    Insert at the end of CLL Efficient Solution

---
+++ Insert at the end of Circular Linked list Efficient Solution.cpp
@@ -0,0 +1,58 @@
+//insert at the end of Circular Linked list Efficient Solution
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+void printList(node *head)
+{
+    if(head==NULL)
+        return;
+        
+    node *p=head;
+    do{
+        cout<<(p->data)<<" ";
+        p=p->next;
+    }while(p!=head);
+    cout<<endl;
+}
+node *insertEnd(node *head, int x)
+{
+    node *temp=new node(x);
+    if(head==NULL)
+    {
+        temp->next=temp;
+        return temp;
+    }
+    else
+    {
+        temp->next=head->next;
+        head->next=temp;
+        int t=head->data;
+        head->data=temp->data;
+        temp->data=t;
+        return temp;
+    }
+}
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=head;
+    int x;
+    cin>>x;
+    printList(head);
+    head=insertEnd(head, x);
+    printList(head);
+    return 0;
+}
