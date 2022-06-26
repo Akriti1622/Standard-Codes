1881dbe629cea232d83f3c5043e126a769b2f6b5
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-27 00:30:01 +0530

    Previous Greater Element - Naive Code

---
+++ Previous Greater Element - Naive Code.cpp
@@ -0,0 +1,30 @@
+// Previous Greater Element - Naive Code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPrevGreater(int arr[],int n){
+    
+    for(int i=0;i<n;i++){
+        int j;
+        for(j=i-1;j>=0;j--){
+            if(arr[j]>arr[i]){
+                cout<<arr[j]<<" ";
+                break;
+            }
+        }
+        if(j==-1)
+            cout<<-1<<" ";
+    }
+}
+
+int main() 
+{ 
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    printPrevGreater(arr,n);
+    return 0; 
+}
