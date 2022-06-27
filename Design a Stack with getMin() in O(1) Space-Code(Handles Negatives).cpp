767230228fd350437cecd540ea24e0445a271d64
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-27 20:46:40 +0530

    Design a Stack with getMin() in O(1) Space -(all)
    Code(Handles Negatives)

---
+++ Design a Stack with getMin() in O(1) Space-Code(Handles Negatives).cpp
@@ -0,0 +1,59 @@
+//Design a Stack with getMin() in O(1) Space - Code(Handles Negatives)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MyStack {
+    stack<int> s;
+    int min;
+  
+void push(int x) {
+ 
+      if(s.empty() ) {
+         min=x;
+         s.push(x);
+      }
+      else if(x<=min){
+          s.push(2*x-min);
+          min=x;
+      }else{
+      s.push(x);
+      }
+   }
+int pop() {
+
+    int t=s.top();s.pop();
+    if(t<=min){
+        int res=min;
+        min=2*min-t;
+        return res;
+    }else{
+        return t;
+    }
+   }
+
+int top() {
+    int t=s.top();
+    return ((t<=min)? min : t);
+   }
+ 
+int getMin() {
+      return min;
+   }
+};
+int main()
+{
+    MyStack s;
+    s.push(4);
+    s.push(3);
+    s.push(5);
+    cout<<s.getMin()<<endl;
+    s.push(8);
+    s.push(2);
+    cout<<s.getMin()<<endl;
+    s.push(1);
+    cout<<s.getMin()<<endl;
+    s.pop();
+    cout<<s.getMin()<<endl;
+    return 0; 
+}
