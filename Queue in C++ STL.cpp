270842717e9137ed49dc682427f356a933a2f120
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-30 19:28:27 +0530

    Queue in C++ STL

---
+++ Queue in C++ STL.cpp
@@ -0,0 +1,22 @@
+//Queue in C++ STL
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() 
+{ 
+    queue <int> q; 
+    q.push(10); 
+    q.push(20); 
+    q.push(30); 
+  
+    cout << q.size()<<endl;
+   while(q.empty() == false)
+   {
+       cout << q.front() << " " << q.back() << endl;
+       q.pop();
+   }
+  
+    return 0; 
+}
