94ae76161a8b67f1f1d30b3c5f961d525a9eb118
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-04 19:49:48 +0530

    Generate numbers with given digits

---
+++ Generate numbers with given digits.cpp
@@ -0,0 +1,34 @@
+//Generate numbers with given digits
+
+#include <bits/stdc++.h> 
+#include <queue>
+using namespace std; 
+
+void printFirstN(int n) 
+{ 
+    queue<string> q;
+    
+    q.push("5");
+    q.push("6");
+    
+    for(int i = 0; i < n; i++)
+    {
+        string curr = q.front();
+       
+        cout << curr << " ";
+        
+        q.pop();
+        
+        q.push(curr + "5");
+        q.push(curr + "6");
+    }
+        
+} 
+
+int main() 
+{ 
+	int n;
+	cin>>n;
+		
+	printFirstN(n);
+} 
