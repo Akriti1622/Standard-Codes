a1c5e1f360e88fdb905d1026442798714f1d3034
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-06 12:35:37 +0530

    Concept of backtracking
    /print all permutation of a given string which do not contain "AB" as a substring - Naive solution

---
+++ Concept of backtracking.cpp
@@ -0,0 +1,31 @@
+//Concept of backtracking
+//print all permutation of a given string which do not contain "AB" as a substring - naive solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void permute(string str, int l, int r){
+    
+    if(l==r){
+        if(str.find("AB")==string::npos){
+            cout<<str<<" ";
+        }
+        return;
+    }else{
+        for(int i=l;i<=r;i++){
+            swap(str[i],str[l]);
+            permute(str,l+1,r);
+            swap(str[i],str[l]);
+        }   
+    }
+}
+
+int main() {
+	
+	string str;
+	getline(cin, str);
+	
+    permute(str,0,str.length()-1);      
+    return 0;  
+	
+}
