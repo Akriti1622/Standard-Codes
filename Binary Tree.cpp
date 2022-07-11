6ef4b68ba8653cb7a47df68f33bfe2ca4d7bb3eb
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-12 00:40:51 +0530

    Binary Tree

---
+++ Binary Tree.cpp
@@ -0,0 +1,23 @@
+//Binary Tree- basic
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+    
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+}
