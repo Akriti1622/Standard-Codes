49a4b3b9650ddcda2098291ad712ef6749d256df
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-21 00:04:36 +0530

    Iterative Preorder Traversal (efficient solution)

---
+++ Iterative Preorder Traversal (efficient solution).cpp
@@ -0,0 +1,42 @@
+//Iterative Preorder Traversal (efficient solution)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void iterativePreorder(Node *root){
+   stack<Node *> s;
+   Node *curr=root;
+   while(curr!=NULL||s.empty()==false)
+   {
+       while(curr!=NULL)
+       {
+           cout<<(curr->key)<<" ";
+           s.push(curr);
+           curr=curr->left;
+       }
+       curr=s.top();
+       s.pop();
+       curr=curr->right;
+   }
+}  
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	iterativePreorder(root);
+}
