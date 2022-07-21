5c16d5676047a25aabf7a3e29b0b4fe1cb12ae80
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-21 15:17:19 +0530

    Search in BST C++ (Recursive Code)

---
+++ Search in BST C++ (Recursive Code).cpp
@@ -0,0 +1,44 @@
+//Search in BST C++ (Recursive Code)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+bool search(Node *root, int x){
+    if(root==NULL)
+        return false;
+    if(root->key==x)
+        return true;
+    else if(root->key>x){
+        return search(root->left,x);
+    }else{
+        return search(root->right,x);
+    }
+} 
+
+int main() {
+	
+	Node *root=new Node(15);
+	root->left=new Node(5);
+	root->left->left=new Node(3);
+	root->right=new Node(20);
+	root->right->left=new Node(18);
+	root->right->left->left=new Node(16);
+	root->right->right=new Node(80);
+	int x;
+	cin>>x;
+	if(search(root,x))
+	    cout<<"Found";
+	else
+	    cout<<"Not Found";
+}
