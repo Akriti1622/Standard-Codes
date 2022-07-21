a849e140ed8ba9ce459b8073c25204ec781bd3e8
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-21 15:17:22 +0530

    Search in BST C++ (Iterative Code)

---
+++ Search in BST C++ (Iterative Code).cpp
@@ -0,0 +1,45 @@
+//Search in BST C++ (Iterative Code)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+bool search(Node *root, int x){
+    while(root!=NULL){
+        if(root->key==x)
+            return true;
+        else if(root->key<x)
+            root=root->right;
+        else
+            root=root->left;
+    }
+    return false;
+} 
+
+int main() {
+	
+	Node *root=new Node(15);
+	root->left=new Node(5);
+	root->left->left=new Node(3);
+	root->right=new Node(20);
+	root->right->left=new Node(18);
+	root->right->left->left=new Node(16);
+	root->right->right=new Node(80);
+	int x;
+	cin>>x;
+	
+	if(search(root,x))
+	    cout<<"Found";
+	else
+	    cout<<"Not Found";
+}
